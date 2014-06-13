9a71ece32a7ae4fe2731090c11ec25aaa5cdb798
Alpy94 <warrior.1994@hotmail.com>
2014-06-13 22:10:15 +0300

    added improved function

--- sieve.hs
+++ sieve.hs
@@ -2,6 +2,7 @@ module Sieve
        where
 
 import Prime
+import Data.List
 
 --This is the implemtation of Sieve of Eratho.
 --http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
@@ -10,10 +11,8 @@ sievelist :: [Int] -> [Int]
 sievelist [] = []
 sievelist (x:xs) = x:sievelist [ k | k <-xs, k `mod` x /=0 ]
 
-
 sieve ::Int -> [Int]
 sieve p = sievelist [2..p]
-
 
 --Function used for testing sieve
 --Functions used for the testing are in Prime.hs
@@ -23,3 +22,12 @@ testSieve = testPrime.sieve
 
 testSieve' :: Int -> Maybe [(Int, Bool)]
 testSieve' = testPrime'.sieve
+
+--A more efficient version of sieve
+
+siev :: [Int] -> [Int]
+siev [] = []
+siev l = let (he, ta) = span (/=head l ^ 2) l
+           in he ++ siev [k | k<-ta, all (/=0) $ map (\ p -> k `mod` p) he ]
+
+erathos = siev [2.. ]
